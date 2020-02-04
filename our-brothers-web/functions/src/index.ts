8964c83e2ad6ae18caa2873637aec0208f9edb2f
Gil Hanan <gil.hanan@gmail.com>
2020-02-04 22:20:26 +0200

    Functions: removeUserFromDatabase and addUserToDatabase

--- our-brothers-web/functions/src/index.ts
+++ our-brothers-web/functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const removeUserFromDatabase = functions.auth.user()
+  .onDelete((event, context) => {
+    console.log('Deleting user ' + event.uid + ' from database.');
+    return admin.database().ref('/users/' + event.uid).remove();
+  });
+
+export const addUserToDatabase = functions.auth.user()
+  .onCreate((event, context) => {
+    console.log('Setting user ' + event.uid + ' registered date to database.');
+    return admin.database().ref('/users/' + event.uid + '/registered')
+      .set(Date.now());
+  });
