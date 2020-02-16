5d6b03c59994ec0f4a9680db66702b9f5edad523
Gil Hanan <gil.hanan@gmail.com>
2020-02-16 14:33:28 +0200

    Team Page: move data from HTML to JSON

--- our-brothers-web/src/app/team-card/team-card.component.ts
+++ our-brothers-web/src/app/team-card/team-card.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
+
+export interface TeamMember {
+  name: string;
+  title: string;
+  frontImage: string;
+  backImage?: string;
+  frontGender?: string;
+  frontBrother?: string;
+  frontBrotherAnd?: string;
+  frontBrother2?: string;
+  frontBrotherLink?: string;
+  frontBrother2Link?: string;
+}
 
 @Component({
   selector: 'app-team-card',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './team-card.component.html',
   styleUrls: ['./team-card.component.scss']
 })
 export class TeamCardComponent implements OnInit {
 
-  @Input() frontImage: string;
-  @Input() frontImage2: string;
-  @Input() frontName: string;
-  @Input() frontTitle: string;
-  @Input() frontGender: string;
-  @Input() frontBrother: string;
-  @Input() frontBrotherAnd: string;
-  @Input() frontBrother2: string;
-  @Input() frontBrotherLink: string;
-  @Input() frontBrother2Link: string;
+  @Input() memeber: TeamMember;
 
   constructor(private router: Router) { }
 
   ngOnInit() { }
 
   moveToPage() {
-    this.router.navigate([this.frontBrotherLink]);
+    this.router.navigate([this.memeber.frontBrotherLink]);
   }
 
   moveToPage2() {
-    this.router.navigate([this.frontBrother2Link]);
+    this.router.navigate([this.memeber.frontBrother2Link]);
   }
 }
