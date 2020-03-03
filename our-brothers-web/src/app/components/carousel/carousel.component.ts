80661806b5527b9521f2cea6a7aae506e59580d2
Yotam Ronen <yotronen@gmail.com>
2020-03-03 22:41:49 +0200

    carousel component

--- our-brothers-web/src/app/components/carousel/carousel.component.ts
+++ our-brothers-web/src/app/components/carousel/carousel.component.ts
@@ -12,14 +12,7 @@ import {
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
-  @Input() slides: TemplateRef<any>[];
-  slidesWithId: { slide: TemplateRef<any>; id: string }[];
-
   constructor() {}
 
-  ngOnInit(): void {
-    this.slidesWithId = this.slides.map((value, index) => {
-      return { slide: value, id: `slide-${index}` };
-    });
-  }
+  ngOnInit(): void {}
 }
